17f6b6308c0ead832c4301b474f582cce7865da6
slapner <bclap4@gmail.com>
2021-09-13 20:49:54 -0400

    oops

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,125 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
+const WORK_HISTORY = [
+	{
+		date: '2008-Current',
+		title: 'Certified Anesthesiologist Assistant',
+		location:
+			'Envision Physician Services, Memorial Hospital West, Pembroke Pines, FL',
+	},
+	{
+		date: '2010-Current',
+		title: 'Student Coordinator',
+		location: 'Nova Southeastern',
+	},
+	{ date: '2005-Current', title: 'Chief', location: 'Allied Health' },
+	{ date: '2003-2005', title: 'Vice-Chief', location: 'Allied Health' },
+];
+
+const EDUCATION = [
+	{
+		date: '2006-2008',
+		title: 'Master of Science: Anesthesia',
+		location: 'Nova Southeastern University - Ft Lauderdale',
+	},
+	{
+		date: '2001-2005',
+		title: 'Bachelor of Science: Health Science',
+		location: 'University of Florida - Gainesville',
+	},
+];
+
+const CERTIFICATIONS = [
+	{
+		date: '2021 August',
+		title: 'ACLS',
+		desc: 'Advanced Cardiac Life Support',
+	},
+	{
+		date: '2021 August',
+		title: 'PALS',
+		desc: 'Pediatric Advanced Life Support',
+	},
+];
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<div className="min-h-screen bg-gray-50 py-6 flex flex-col justify-center sm:py-12">
+			<Head>
+				<title>Jillian Sabates - Certified Anesthesiologist Assistant</title>
+			</Head>
+			<div className="relative py-3 sm:max-w-xl sm:mx-auto">
+				<div className="">
+					<div className="max-w-md mx-auto">
+						<div className="space-y-2">
+							<h1 className="text-4xl font-bold text-gray-700">
+								Jillian Sabates
+							</h1>
+							<h2 className="text-gray-600 font-light">
+								Certified Anesthesiologist Assistant
+							</h2>
+							<h3 className="text-base font-semibold tracking-tight text-gray-600 space-x-4">
+								<span>(941) 400-7522</span>
+								<span>jclapner@gmail.com</span>
+							</h3>
+						</div>
+						<div className="divide-y divide-gray-200">
+							<div className="py-8 text-sm leading-6 space-y-4 text-gray-700 sm:text-base">
+								<p>
+									I am an extremely experienced Anesthesiologist Assistant,
+									currently serving as the Chief and Department Leader of Allied
+									Health Department
+								</p>
+							</div>
+							<div className="py-8 text-sm leading-6 space-y-4 text-gray-700 sm:text-base">
+								<h2 className="font-bold text-xl">Work History</h2>
+								{WORK_HISTORY.map((item) => (
+									<div className="flex w-full" key={item.date}>
+										<div className="font-bold w-2/5 text-sm">{item.date}</div>
+										<div className="flex flex-col flex-1">
+											<div className="font-bold text-sm">{item.title}</div>
+											<div className="text-sm italic">{item.location}</div>
+										</div>
+									</div>
+								))}
+							</div>
+							<div className="py-8 text-sm leading-6 space-y-4 text-gray-700 sm:text-base">
+								<h2 className="font-bold text-xl">Education</h2>
+								{EDUCATION.map((item) => (
+									<div className="flex w-full" key={item.date}>
+										<div className="font-bold w-2/5 text-sm">{item.date}</div>
+										<div className="flex flex-col flex-1">
+											<div className="font-bold text-sm">{item.title}</div>
+											<div className="text-sm italic">{item.location}</div>
+										</div>
+									</div>
+								))}
+							</div>
+							<div className="py-8 text-sm leading-6 space-y-4 text-gray-700 sm:text-base">
+								<h2 className="font-bold text-xl">Certifications</h2>
+								{CERTIFICATIONS.map((item) => (
+									<div className="flex w-full" key={item.date}>
+										<div className="font-bold w-2/5 text-sm">{item.date}</div>
+										<div className="flex flex-col flex-1">
+											<div className="font-bold text-sm">{item.title}</div>
+											<div className="text-sm italic">{item.desc}</div>
+										</div>
+									</div>
+								))}
+							</div>
+							<div className="py-8 text-sm leading-6 space-y-4 text-gray-700 sm:text-base">
+								<h2 className="font-bold text-xl">Affiliations</h2>
+								<div className="space-y-2">
+									<div>Florida Society of Anesthesiologists</div>
+									<div>Florida Academy of Anesthesiologist</div>
+									<div>Assistants American Society of Anesthesiologists</div>
+									<div>American Academy of Anesthesiologist Assistants</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
